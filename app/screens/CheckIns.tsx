e40a7a140d3a1d2805b66c8715c4974a4fedc693
Namrata65 <namrata.aeri@gmail.com>
2025-02-24 03:40:37 -0800

    sevya mobile routing flow

---
+++ app/screens/CheckIns.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CheckIns = () => {
+  return (
+    <View>
+      <Text>CheckIns</Text>
+    </View>
+  )
+}
+
+export default CheckIns
+
+const styles = StyleSheet.create({})
---
+++ app/screens/Shifts.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Shifts = () => {
+  return (
+    <View>
+      <Text>Shifts</Text>
+    </View>
+  )
+}
+
+export default Shifts
+
+const styles = StyleSheet.create({})
