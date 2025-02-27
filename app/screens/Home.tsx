f83f325f7f3a0a31381e230076814e71a818d581
Amneesh <amneeshsingh5@gmail.com>
2025-02-26 19:25:09 -0800

    SCRUM-103-structure-setup

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <View>
-      <Text style={{ color: 'black', fontSize: 20, textAlign: 'center', margin: 10, padding: 10, marginVertical: 100 }}>Welcome to the Home Screen</Text>
+      <Text style={{ color: 'black', fontSize: 20, textAlign: 'center', margin: 10, padding: 10, marginVertical: 100 }}>Welcome to the Sevya</Text>
     </View>
   )
 }
