043a2455e69b348eccd303782a0b06601b2853c7
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2025-03-18 15:17:54 -0700

    SCRUM-161-Login screen UI updates

--- app/login.tsx
+++ app/login.tsx
@@ -3,19 +3,19 @@ import {
   View,
   Text,
   TextInput,
+  ImageBackground,
   TouchableOpacity,
   Alert,
   StyleSheet,
 } from "react-native";
-import Button from "@/components/ui/Button";
-
+import Button from "../components/ui/Button";
 import { useRouter } from "expo-router";
 import axios from "axios";
 import { auth } from "../config/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { saveSecureData, getSecureData } from "../services/secureStorage"; // Import Seure Storage utility
+import { saveSecureData, getSecureData } from "../services/secureStorage";
 
-const API_URL = "http://192.168.1.212:8800/api";
+const API_URL = "http://localhost:8800/api";
 
 const LoginScreen: React.FC = () => {
   const [email, setEmail] = useState<string>("nammy@caregiver.com");
@@ -23,28 +23,15 @@ const LoginScreen: React.FC = () => {
   const router = useRouter();
 
   const handleLogin = async () => {
-
     try {
-<<<<<<< HEAD
-      router.push("/(tabs)/patients");
-      // const userCredential = await signInWithEmailAndPassword(
-      //   auth,
-      //   email,
-      //   password
-      // );
-      // const idToken = await userCredential.user.getIdToken();
-      // console.log("Firebase Token:", idToken);
-      // await authenticate(idToken);
-=======
       const userCredential = await signInWithEmailAndPassword(
         auth,
         email,
         password
       );
       const idToken = await userCredential.user.getIdToken();
-      // console.log("Firebase Token:", idToken);
       await authenticate(idToken);
->>>>>>> main
+      router.push("/(tabs)/home");
     } catch (error) {
       console.error("Login Error:", error);
       Alert.alert("Error", "Invalid email or password");
@@ -58,9 +45,6 @@ const LoginScreen: React.FC = () => {
         collectionName: "caregivers",
       });
 
-      // console.log("Full data received from API:", data);
-
-      // Securely store token and user data
       await saveSecureData("token", data.token);
       await saveSecureData("user", JSON.stringify(data.user));
 
@@ -68,11 +52,10 @@ const LoginScreen: React.FC = () => {
       if (!first_time_login) {
         router.replace("/screens/InfoScreen");
       } else {
-        router.replace("/(tabs)/patients");
+        router.replace("/(tabs)/home");
       }
 
       Alert.alert("Success", "Login Successful");
-      console.log("Login Successful:", data);
     } catch (error: any) {
       console.error("Authentication Error:", error);
       Alert.alert("Error", "Failed to authenticate user.");
@@ -80,59 +63,78 @@ const LoginScreen: React.FC = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <View style={styles.form}>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          style={styles.input}
-        />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-          style={styles.input}
-        />
-  
-      <Button
-          handleButtonClick={handleLogin}
-          buttonText="Login"
-          disabled={false}
-        />
-      
+    <ImageBackground
+      source={require("../assets/main-bg.png")}
+      style={styles.background}
+    >
+      <View style={styles.overlay}>
+        <View style={styles.container}>
+          <Text style={styles.title}>Sign in with E-mail</Text>
+          <TextInput
+            placeholder="Enter your e-mail"
+            value={email}
+            onChangeText={setEmail}
+            style={styles.input}
+            placeholderTextColor="#ccc"
+          />
+          <TextInput
+            placeholder="Enter password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            style={styles.input}
+            placeholderTextColor="#ccc"
+          />
+          <Button
+            handleButtonClick={handleLogin}
+            style={styles.loginButton}
+            buttonText="Login"
+            disabled={false}
+          />
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
+    resizeMode: "cover",
+  },
+  loginButton: {
+    backgroundColor: "#4CAF50", // Green color
+    paddingVertical: 12,
+    paddingHorizontal: 80,
+    borderRadius: 50,
+    alignItems: "center",
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(7, 24, 50, 0.7)",
     justifyContent: "center",
     alignItems: "center",
-    padding: 20,
   },
-  title: { fontSize: 24, fontWeight: "bold", marginBottom: 20 },
-  form:{
-padding:40,
-    width: '100%',
+  container: {
+    width: "85%",
+    padding: 20,
+    borderRadius: 15,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
     marginBottom: 20,
-
-
   },
   input: {
-
     width: "100%",
-    padding: 10,
-    borderWidth: 1,
-    borderRadius: 100,
-    marginBottom: 10,
+    padding: 15,
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    borderRadius: 25,
+    marginBottom: 15,
+    textAlign: "center",
   },
-  button: { backgroundColor: "blue", padding: 10, borderRadius: 5 },
-  buttonText: { color: "white" },
 });
 
 export default LoginScreen;
