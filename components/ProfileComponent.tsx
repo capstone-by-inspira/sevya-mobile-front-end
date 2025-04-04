950bd1ad6e195bc9aa6e0a4dc1e6c88d1d7502f8
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2025-04-04 02:32:15 -0700

    SCRUM-344-Setting profile image updated fixed

--- components/ProfileComponent.tsx
+++ components/ProfileComponent.tsx
@@ -1,14 +1,21 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, ActivityIndicator } from 'react-native';
-import { AppContext } from './AppContext'; // Adjust path as needed
-import { getSecureData } from '../services/secureStorage'; // Import secure storage
-import * as ImagePicker from 'expo-image-picker';
-import { updateDocument } from '../services/api'; // Import your uploadImage and updateDocument functions
-import { auth } from '@/config/firebase';
-import { Icon } from 'react-native-paper';
-import { formatDateOnly, formatLocalDate } from '@/services/utils';
+import React, { useContext, useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
+import { AppContext } from "./AppContext"; // Adjust path as needed
+import { getSecureData } from "../services/secureStorage"; // Import secure storage
+import * as ImagePicker from "expo-image-picker";
+import { updateDocument } from "../services/api"; // Import your uploadImage and updateDocument functions
+import { auth } from "@/config/firebase";
+import { Icon } from "react-native-paper";
+import { formatDateOnly, formatLocalDate } from "@/services/utils";
+import axios from "axios";
 const ProfileScreen = () => {
-
   const context = useContext(AppContext);
 
   if (!context) {
@@ -22,8 +29,8 @@ const ProfileScreen = () => {
 
   useEffect(() => {
     const fetchProfileImage = async () => {
-      if (caregivers && caregivers.profileImage) {
-        setProfileImage(caregivers.profileImage);
+      if (caregivers && caregivers.image) {
+        setProfileImage(caregivers.image);
       }
     };
 
@@ -32,7 +39,7 @@ const ProfileScreen = () => {
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ["images"],
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
@@ -40,34 +47,68 @@ const ProfileScreen = () => {
 
     if (!result.canceled && result.assets && result.assets.length > 0) {
       setLoading(false);
-      //   try {
-      //     const uploadResult = await uploadImage(result.assets[0].uri, `profileImages/${userData.uid}`, token);
-
-      //     if (uploadResult.success && uploadResult.url) {
-      //       setProfileImage(uploadResult.url);
-      //       await updateUserData(uploadResult.url);
-      //     } else {
-      //       console.error("Image upload failed:", uploadResult.error);
-      //     }
-      //   } catch (error) {
-      //     console.error("Error picking and uploading image:", error);
-      //   } finally {
-      //     setLoading(false);
-      //   }
+      setProfileImage(result.assets[0].uri);
+      let imageUrl = null;
+      if (profileImage) {
+        setLoading(true);
+        try {
+          imageUrl = await uploadImage(result.assets[0].uri);
+          if (imageUrl) {
+            await updateUserData(imageUrl);
+            setLoading(false);
+          } else {
+            console.error("Image upload failed, no URL returned.");
+          }
+        } catch (error) {
+          console.error("Error uploading image:", error);
+        }
+      }
     }
   };
 
   const updateUserData = async (imageUrl: string) => {
     if (caregivers && token) {
       try {
-        await updateDocument('caregivers', caregivers.uid, { profileImage: imageUrl }, token);
-        const updatedUser = { ...caregivers, profileImage: imageUrl };
-        fetchData()
+        await updateDocument(
+          "caregivers",
+          caregivers.uid,
+          { image: imageUrl },
+          token
+        );
+        const updatedUser = { ...caregivers, image: imageUrl };
+        fetchData();
         // await getSecureData("user", JSON.stringify(updatedUser));
         // fetchData();
       } catch (error) {
         console.error("Error updating user data:", error);
       }
+    }
+  };
+
+  const uploadImage = async (uri: string) => {
+    const mainLink = "https://sevya-admin.site:8808/api/auth/upload";
+
+    try {
+      const formData = new FormData();
+      formData.append("image", {
+        uri,
+        name: `image_${Date.now()}.jpg`,
+        type: "image/jpeg",
+      } as any);
+      const uploadResponse = await axios.post(mainLink, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (uploadResponse.status !== 200) {
+        throw new Error("Image upload failed.");
+      }
+
+      return uploadResponse.data.imageUrl || null;
+    } catch (error) {
+      console.error("Image upload error:", error);
+      return null;
     }
   };
 
@@ -90,39 +131,49 @@ const ProfileScreen = () => {
 
       <View style={styles.imageContainer}>
         <Image
-          source={profileImage ? { uri: profileImage } : require('../assets/images/placeholder-image.jpg')} // Replace with your placeholder
+          source={
+            profileImage
+              ? { uri: profileImage }
+              : require("../assets/images/placeholder-image.jpg")
+          } // Replace with your placeholder
           style={styles.profileImage}
         />
-        <Text style={styles.changeImageText} onPress={pickImage}>
-          Change Profile Image  
-          <Icon source="pencil" size={18} color="#1E3A8A" />
-        </Text>
+        <TouchableOpacity onPress={pickImage}>
+          <Text style={styles.changeImageText}>
+            Change Profile Image
+            <Icon source="pencil" size={18} color="#1E3A8A" />
+          </Text>
+        </TouchableOpacity>
       </View>
 
       <View style={styles.detailsContainer}>
         <Text style={styles.detailText}>
-          <Text style={{ fontWeight: "bold" }}>Name:</Text> {caregivers.firstName} {caregivers.lastName}
+          <Text style={{ fontWeight: "bold" }}>Name:</Text>{" "}
+          {caregivers.firstName} {caregivers.lastName}
         </Text>
         <Text style={styles.detailText}>
           <Text style={{ fontWeight: "bold" }}>Email:</Text> {caregivers.email}
         </Text>
         <Text style={styles.detailText}>
-          <Text style={{ fontWeight: "bold" }}>Phone:</Text> {caregivers.phoneNumber}
+          <Text style={{ fontWeight: "bold" }}>Phone:</Text>{" "}
+          {caregivers.phoneNumber}
         </Text>
         <Text style={styles.detailText}>
-          <Text style={{ fontWeight: "bold" }}>Total Shifts:</Text> {shifts?.length}
+          <Text style={{ fontWeight: "bold" }}>Total Shifts:</Text>{" "}
+          {shifts?.length}
         </Text>
         <Text style={styles.detailText}>
-          <Text style={{ fontWeight: "bold" }}>Total Patients Assigned:</Text> {patients.length}
+          <Text style={{ fontWeight: "bold" }}>Total Patients Assigned:</Text>{" "}
+          {patients.length}
         </Text>
 
         <Text style={styles.detailText}>
-          <Text style={{ fontWeight: "bold" }}>Availability:</Text> {formatLocalDate(caregivers.availability)}
+          <Text style={{ fontWeight: "bold" }}>Availability:</Text>{" "}
+          {formatLocalDate(caregivers.availability)}
         </Text>
 
         {/* Add other user details here */}
       </View>
-
     </View>
   );
 };
@@ -135,17 +186,17 @@ const styles = StyleSheet.create({
     paddingTop: 80,
     paddingHorizontal: 0,
     marginHorizontal: 0,
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
   },
   loadingOverlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'transparent',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
     zIndex: 10,
   },
   imageContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 20,
   },
   profileImage: {
@@ -154,28 +205,27 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 200,
     borderWidth: 6,
-    borderColor: "#10B981", 
+    borderColor: "#10B981",
     backgroundColor: "#FFF",
     boxShadow:
       "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
   },
   changeImageText: {
-    color: '#25578E',
+    color: "#25578E",
     marginTop: 5,
     borderWidth: 1,
-    borderColor: '#25578E',
+    borderColor: "#25578E",
     padding: 10,
     borderRadius: 24,
   },
   detailsContainer: {
     marginTop: 20,
     borderRadius: 10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 20,
     width: 400,
     boxShadow:
       "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
-
   },
   detailText: {
     fontSize: 16,
